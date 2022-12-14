2c6bc10a7702be43fd144d6b9649683973962049
GutoAS <agostinhosoberano2@gmail.com>
2022-12-14 10:10:53 +0200

    Real time Collection data

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
   deleteDoc,
   getFirestore,
   doc,
+  onSnapshot,
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -27,18 +28,26 @@ const db = getFirestore();
 //collection ref
 const colRef = collection(db, "books");
 
-//get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
-  .catch((err) => {
-    console.log(err.message);
+onSnapshot(colRef, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
   });
+  console.log(books);
+});
+
+// // get collection data
+// // getDocs(colRef)
+// //   .then((snapshot) => {
+// //     let books = [];
+// //     snapshot.docs.forEach((doc) => {
+// //       books.push({ ...doc.data(), id: doc.id });
+// //     });
+// //     console.log(books);
+// //   })
+// //   .catch((err) => {
+// //     console.log(err.message);
+// //   });
 
 // adding docs
 const addBookForm = document.querySelector(".add");
